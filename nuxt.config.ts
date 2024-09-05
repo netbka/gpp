7f652820beb1863c541a028ac35fdcfb972b5e7b
Peter <netbka@gmail.com>
2024-09-05 11:11:09 +0300

    default RU

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,7 +62,7 @@ export default defineNuxtConfig({
         dir: "rtl",
       },
     ],
-    defaultLocale: "en-US",
+    defaultLocale: "ru-RU",
     detectBrowserLanguage: {
       useCookie: true,
       cookieKey: "gpp_i18n",
@@ -124,8 +124,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     dbPassword: process.env.DB_PASSWORD,
     public: {
-      baseUrl: process.env.NODE_ENV === "development" ? "https://localhost:7094/api" : "http://mamrukov-001-site1.ktempurl.com/api", // "https://apiofp.mamrukov.com/api", //"https://apiofp.runasp.net/api",
-      wwwwUrl: process.env.NODE_ENV === "development" ? "https://localhost:7094" : "http://mamrukov-001-site1.ktempurl.com", //https://apiofp.mamrukov.com",
+      baseUrl: process.env.NODE_ENV === "development" ? "https://localhost:7094/api" : "https://apiofp.mamrukov.com/api", //"https://apiofp.runasp.net/api",
+      wwwwUrl: process.env.NODE_ENV === "development" ? "https://localhost:7094" : " https://apiofp.mamrukov.com",
     },
   },
 
